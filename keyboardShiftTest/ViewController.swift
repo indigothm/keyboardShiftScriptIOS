3fbfb710560e4205c3c8339f05e6230c4d8f994a
Ilia Tikhomirov <iliatikhomirov@Ilias-MacBook-Air.local>
2015-09-10 17:33:16 +0300

    Initial Commit

---
+++ keyboardShiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  keyboardShiftTest
+//
+//  Created by Ilia Tikhomirov on 10/09/15.
+//  Copyright (c) 2015 Ilia Tikhomirov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
